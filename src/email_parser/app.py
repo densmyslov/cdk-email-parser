cb26ee08f7cbad66570476ba0ea62cbbceeb9e68
Denis Smyslov <dsmyslov@protonmail.com>
2024-11-01 11:29:02 +0000

    Added client_email print

--- src/email_parser/app.py
+++ src/email_parser/app.py
@@ -20,9 +20,10 @@ s3_client = boto3.client('s3')
 def lambda_handler(event, context):
     # TODO implement
     
-    # print(event)
+    print(event)
     # event is passed from invPar-email-parser-feeder
     client_email = event[0]
+    print(f"client_email: {client_email}")
     client_id = event[1]
     service_email = event[2]
     email_key = event[3]
