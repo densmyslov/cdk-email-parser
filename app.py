a8b5ca1aeb7da479f85e85467f66cc3347a601a2
Denis Smyslov <dsmyslov@protonmail.com>
2024-11-01 06:16:18 +0000

    Rearranged lines to look nicer

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import os
 import aws_cdk as cdk
 
 from cdk_email_parser.cdk_email_parser_stack import CdkEmailParserStack
+from aws_cdk import App, Environment
+from cdk_email_parser.cdk_email_parser_stack import CdkEmailParserStack  # Your stack import
 
 
-app = cdk.App()
-CdkEmailParserStack(app, "CdkEmailParserStack",
+
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -15,7 +16,13 @@ CdkEmailParserStack(app, "CdkEmailParserStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+prod_env=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), 
+                             region=os.getenv('CDK_DEFAULT_REGION'))
+
+stage_env=Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), 
+                             region='us-east-2')
+
+    # Instantiate stacks for each environment
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
@@ -23,6 +30,11 @@ CdkEmailParserStack(app, "CdkEmailParserStack",
     #env=cdk.Environment(account='123456789012', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+
+app = App()
+
+# Instantiate stacks for each environment
+CdkEmailParserStack(app, "CdkEmailParserStack-Stage", env=stage_env)
+CdkEmailParserStack(app, "CdkEmailParserStack-Prod", env=prod_env)
 
 app.synth()
